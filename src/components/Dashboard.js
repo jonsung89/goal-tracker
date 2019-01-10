1eb562cca2d9cf0bb3723b28b3f02c0f72f6c250
Jon Sung <jonsung89@gmail.com>
2019-01-09 22:30:55 -0600

    Dashboard shows no goals when there are no goals

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,25 +5,53 @@ export default class Dashboard extends Component {
   constructor(props){
     super(props);
     this.state = {
-      goals: ['Read 30 books this year']
+      // goals: ['Read 30 books this year']
+      goals: []
     }
   }
 
   updateGoals(goal) {
-    console.log('goal is: ', goal);
-    this.setState({
-      goals: [...this.state.goals, goal]
-    })
+    // console.log('goal is: ', goal);
+    if (this.state.goals === null) {
+      this.setState({
+        goals: goal
+      })  
+    } else {
+      this.setState({
+        goals: [...this.state.goals, goal]
+      })
+    }
   }
 
   render() {
     const {goals} = this.state;
 
-    const listGoal = goals.map((goal, i) => {
-      return (
-        <div className="goal-box" key={i}>{goal}</div>
+    let dashboardContent;
+
+    if (goals.length > 0) {
+      const listGoal = goals.map((goal, i) => {
+        return (
+          <div className="goal-box" key={i}>{goal}</div>
+        )
+      })
+      dashboardContent = (
+        <div className="goal-list">
+          <div className="my-goals">
+            <span>MY GOALS<div className="bottom-border"></div></span>
+          </div>
+          {listGoal}
+        </div>
       )
-    })
+    } else {
+      dashboardContent = (
+        <div className="goal-list">
+          <div className="my-goals">
+            <span>MY GOALS<div className="bottom-border"></div></span>
+          </div>
+          <p>No Goals</p>
+        </div>
+      )
+    }
 
     return (
       <div className="dashboard">
@@ -31,12 +59,7 @@ export default class Dashboard extends Component {
           <h3>What would you like to accomplish?</h3>
           <Form triggerUpdate={(e) => this.updateGoals(e)} />
         </div>
-        <div className="goal-list">
-          <div className="my-goals">
-            <span>MY GOALS<div className="bottom-border"></div></span>
-          </div>
-          {listGoal}
-        </div>
+        {dashboardContent}
       </div>
     )
   }
