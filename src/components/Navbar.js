b7af0eed6186a5f71d19b7558e51f5aefda60a8b
Jon Sung <jonsung89@gmail.com>
2019-02-10 11:48:56 -0600

    navbar responsiveness complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,19 @@ export default class Navbar extends Component {
       <header className="header">
         <div className="box-left">
           <div className="ico-menu">
-            <div className="bar"></div>
-            <div className="bar"></div>
-            <div className="bar"></div>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
           </div>
-          <div className="item">MENU</div>
+          <div className="menu">MENU</div>
         </div>
         <div className="logo-header">
           <h1>Goal Tracker<span className="secondary-color">.</span></h1>
         </div>
-        <div className="box-right"></div>
+        <div className="box-right">
+          <div className="add"></div>
+          <div className="add-large"><div>ADD A GOAL</div></div>
+        </div>
       </header>
     )
   }
